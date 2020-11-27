6edd8cceabe012a03158a59269d733e6dc6f5a42
zidane-sc <zidankren02@gmail.com>
2020-11-27 13:30:31 +0700

    fix auth in web route

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'admin'], function () {
     Auth::routes();
 
-<<<<<<< HEAD
     Route::get('/dashboard', function () {
         return view('server.dashboard');
-    });
+    })->middleware('auth');
 
-=======
-    Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> fc91d0ebc4fce7dfc71dd6a28d1b91e2e30c8cfc
 });
 
 
@@ -35,8 +31,6 @@ Route::group(['prefix' => 'admin'], function () {
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'MainController@index')->name('main.index');
